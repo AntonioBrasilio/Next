fdbdcc2d37f71e3e52c3e24db5f6c842d1258fc7
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-26 18:44:17 -0300

    feat: Organizing components

---
+++ meals-project/components/main-header/main-header.js
@@ -0,0 +1,35 @@
+import logo from '@/assets/logo.png';
+import Link from 'next/link';
+import classes from './main-header.module.css';
+import Image from 'next/image';
+import { MainHeaderBackground } from './main-header-background';
+
+export const MainHeader = () => {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link
+                    className={classes.logo}
+                    href="/">
+                    <Image
+                        src={logo}
+                        alt="A plate with food on it"
+                        priority
+                    />
+                    NextLevel Food
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Broswe Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Foodies community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+};
