f4d59b9b817b38ef86aea5ae17ae4c1b8d070ee8
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-27 15:55:05 -0300

    feat: Using granular loading

--- meals-project/app/meals/page.js
+++ meals-project/app/meals/page.js
@@ -2,10 +2,15 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import { MealsGrid } from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+import MealsLoading from './loading-out';
 
-export default function MealsPage() {
-    const meals = getMeals();
-    
+async function Meals() {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -18,7 +23,9 @@ export default function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<MealsLoading />}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
