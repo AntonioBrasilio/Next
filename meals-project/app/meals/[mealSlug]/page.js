541fc4067c49d9b8df235421b4534c549ee6d79c
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-27 18:03:56 -0300

    feat: Meals detail page

--- meals-project/app/meals/[mealSlug]/page.js
+++ meals-project/app/meals/[mealSlug]/page.js
@@ -1,7 +1,33 @@
-export default function OutletPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+
     return (
-        <div>
-            <h1>Outlet Page</h1>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image
+                        src={meal.image}
+                        alt={meal.title}
+                        fill
+                    />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main className={classes.main}>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+            </main>
+        </>
     );
 }
