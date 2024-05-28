f933840e3b395621d1c9503a7decd916a1ac4d3a
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-27 22:18:32 -0300

    feat: Protecting against xss

--- meals-project/lib/meals.js
+++ meals-project/lib/meals.js
@@ -1,10 +1,18 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
 export const getMeals = async () => {
     return db.prepare('SELECT * FROM meals').all();
 };
+
 export const getMeal = (slug) => {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+};
+
+export const saveMeal = (meal) => {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
 };
