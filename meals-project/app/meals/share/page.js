16e03df3a365879fbf1b4dcaf06211de47698956
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-27 21:56:39 -0300

    feat? Using handle submission

--- meals-project/app/meals/share/page.js
+++ meals-project/app/meals/share/page.js
@@ -1,5 +1,8 @@
+'use client';
+
 import { ImagePicker } from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { submitShare } from '@/lib/action';
 
 export default function ShareMealPage() {
     return (
@@ -11,7 +14,9 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form
+                    className={classes.form}
+                    action={submitShare}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
