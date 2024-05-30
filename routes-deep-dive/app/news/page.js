d83e3d96b29cd71a5e41c3266d58cef2f0d3ad71
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-30 16:03:56 -0300

    feat: Basic functions

--- routes-deep-dive/app/news/page.js
+++ routes-deep-dive/app/news/page.js
@@ -4,9 +4,17 @@ export default function NewsPage() {
     return (
         <div>
             <h1>News Page</h1>
-            <Link href="/news/first-news-item">First News Item</Link>
-            <Link href="/news/second-news-item">Second News Item</Link>
-            <Link href="/news/third-news-item">Third News Item</Link>
+            <ul className="news-list">
+                <li>
+                    <Link href="/news/first-news-item">First News Item</Link>
+                </li>
+                <li>
+                    <Link href="/news/second-news-item">Second News Item</Link>
+                </li>
+                <li>
+                    <Link href="/news/third-news-item">Third News Item</Link>
+                </li>
+            </ul>
         </div>
     );
 }
