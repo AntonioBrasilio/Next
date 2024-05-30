ddc506c31c3613be3dee77655b44bc141cab4cf8
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-30 18:44:37 -0300

    feat: News styles made

--- routes-deep-dive/app/news/page.js
+++ routes-deep-dive/app/news/page.js
@@ -1,19 +1,24 @@
 import Link from 'next/link';
 
+import { DUMMY_NEWS } from '@/dummy-news';
+
 export default function NewsPage() {
     return (
         <div>
             <h1>News Page</h1>
             <ul className="news-list">
-                <li>
-                    <Link href="/news/first-news-item">First News Item</Link>
-                </li>
-                <li>
-                    <Link href="/news/second-news-item">Second News Item</Link>
-                </li>
-                <li>
-                    <Link href="/news/third-news-item">Third News Item</Link>
-                </li>
+                {DUMMY_NEWS &&
+                    DUMMY_NEWS.map((newsItem) => (
+                        <li key={newsItem.id}>
+                            <Link href={`/news/${newsItem.slug}`}>
+                                <img
+                                    src={`/images/news/${newsItem.image}`}
+                                    alt={newsItem.title}
+                                />
+                                <span>{newsItem.title}</span>
+                            </Link>
+                        </li>
+                    ))}
             </ul>
         </div>
     );
