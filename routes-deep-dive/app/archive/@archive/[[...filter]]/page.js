5d15502f791f07c6bd4e53e9c7fb911992a49cb1
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-01 17:48:11 -0300

    feat: Handling catch-all-routes year

--- routes-deep-dive/app/archive/@archive/[[...filter]]/page.js
+++ routes-deep-dive/app/archive/@archive/[[...filter]]/page.js
@@ -3,22 +3,40 @@ import { getAvailableNewsYears, getNewsForYear } from '@/lib/news';
 import Link from 'next/link';
 
 export default function YearNewsPage({ params }) {
-    const filter = getNewsForYear(params.filter);
+    const filter = params.filter;
+
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+    }
+
+    let newsContent = <p>No news found for the selected period!</p>;
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
 
     const links = getAvailableNewsYears();
 
     return (
-        <header id="archive-header">
-            <nav>
-                <ul>
-                    {links &&
-                        links.map((link) => (
-                            <li key={link}>
-                                <Link href={`/archive/${link}`}>{link}</Link>
-                            </li>
-                        ))}
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {links &&
+                            links.map((link) => (
+                                <li key={link}>
+                                    <Link href={`/archive/${link}`}>{link}</Link>
+                                </li>
+                            ))}
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
     );
 }
