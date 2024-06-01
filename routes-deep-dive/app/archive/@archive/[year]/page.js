0916b14b2343cda9e8bff34e03d7a7a06c57c212
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-01 16:52:44 -0300

    feat: Basic setup for catch-all-routes

--- routes-deep-dive/app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import { NewsList } from '@/components/news-list';
-import { getNewsForYear } from '@/lib/news';
-
-export default function YearNewsPage({ params }) {
-    const news = getNewsForYear(params.year);
-
-    return <NewsList news={news} />;
-}
