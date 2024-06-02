85ad6c230efaafe69525f99bc8d0095c1e65ac36
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-01 23:04:10 -0300

    fix: Right message

--- routes-deep-dive/app/archive/@archive/[[...filter]]/error.js
+++ routes-deep-dive/app/archive/@archive/[[...filter]]/error.js
@@ -1,10 +1,10 @@
 'use client';
 
-export default function FilterError() {
+export default function FilterError({ error }) {
     return (
         <div id="error">
             <h2>An error ocurred!</h2>
-            <p>Invalid filter!</p>
+            <p>{error.message}</p>
         </div>
     );
 }
